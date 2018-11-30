2847048af6d30d846fc63d283a1b9a3a72a01c10
Ishaan Agrawal <ia2341@columbia.edu>
2018-11-30 11:12:57 -0500

    Added star wars trained model to the list of all weights

--- DeepQA_with_attention/chatbot/chatbot.py
+++ DeepQA_with_attention/chatbot/chatbot.py
@@ -113,7 +113,7 @@ class Chatbot:
 
         # Training options
         trainingArgs = parser.add_argument_group('Training options')
-        trainingArgs.add_argument('--numEpochs', type=int, default=75, help='maximum number of epochs to run')
+        trainingArgs.add_argument('--numEpochs', type=int, default=30, help='maximum number of epochs to run')
         trainingArgs.add_argument('--saveEvery', type=int, default=2000, help='nb of mini-batch step before creating a model checkpoint')
         trainingArgs.add_argument('--batchSize', type=int, default=256, help='mini-batch size')
         trainingArgs.add_argument('--learningRate', type=float, default=0.002, help='Learning rate')
