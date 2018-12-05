eaf486926a22f0bee1356c37a228d2207d884b00
sunandiyer <sunand@sunand.com>
2018-12-04 23:57:56 -0500

    Adding Darth Vader Script generation

---
+++ main.py
@@ -0,0 +1,12 @@
+import get_vader_lines as gvl
+
+def main():
+
+	gvl.createLines()
+	gvl.createConversations()
+	gvl.createVaderConversations()
+	gvl.getVaderLines()
+	gvl.get_225()
+	gvl.getVaderConvos()
+
+main()
